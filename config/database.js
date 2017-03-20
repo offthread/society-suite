8a663a222126a381529386167803c68c2e552fec
Felipe Vieira <felipe29vieira@gmail.com>
2017-03-20 16:05:07 -0300

    Updating local database credentials

--- config/database.js
+++ config/database.js
@@ -3,7 +3,7 @@ module.exports = {
     'connection': {
         'host': 'localhost',
         'user': 'root',
-        'password': 'prologo'
+        'password': 'test'
     },
 	'database': 'rda_schema',
     'users_table': 'users'
